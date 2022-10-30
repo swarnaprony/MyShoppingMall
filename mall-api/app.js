33f55d4c347f0eac4dcffe11ab555a7540c50c85
Swarna Sarker <swarnaprony81@gmail.com>
2022-10-30 18:37:18 +0100

    Add app.js in mall-api and indx.html in mall-ui

--- mall-api/app.js
+++ mall-api/app.js
@@ -1,4 +1,14 @@
-const express = require('express');
+const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const cors = require('cors');
+const ejs = require("ejs");
+
+const app = express();
+
+app.get('/', function(req, res){
+    res.send("Welcome to Shopping Mall!")
+});
+
+
+app.listen(3000, function(){
+    console.log("Server Started on Port 3000");
+});
